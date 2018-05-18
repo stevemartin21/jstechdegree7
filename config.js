e08c20e235cef0d10ebfbd5086bdfdb94ba5b463
Steve Martin <stevemartinrealestate@gmail.com>
2018-05-18 16:25:42 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports ={
+
+  consumer_key:         'PxjUNJFDF9GrAgGVceK3THsL2',
+  consumer_secret:      '7rvJLqtr0dnBtxRG1JgV2KrvrMxrUxGY2wGZtZP6Xs52XyZiC1',
+  access_token:         '2944270142-hbAXCGZcWvP0RJIrhbhBzGOGGHDSHwiC6uOsGv3',
+  access_token_secret:  'qWvBSUr26OvsHzXor8IRQDr7D4Y86otVvuo2Oj3KBlUcF',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+
+
+}
